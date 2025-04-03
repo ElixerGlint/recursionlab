a2018274e94caf4bfb64168cb6b11fae56c63233
Elixer <everett.rumery@gmail.com>
2025-04-03 13:53:42 -0400

    safasgasg

--- src/WordDriver.java
+++ src/WordDriver.java
@@ -1,10 +1,21 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class WordDriver {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
+        Scanner input = new Scanner(System.in); //creating a scanner
+        System.out.println("What is the name of your wordsearch file (make sure to NOT include .txt): "); //asking for the file names (used later to pull and push data)
+        String file1 = input.next();
+        file1+=".txt";
+        Scanner fileinput = new Scanner(new File(file1)); //creating the reader
+        WordSearch wordsearch = new WordSearch(fileinput);
+        fileinput.close();
+        input.close();
+        
 
-
-
+        System.out.println(wordsearch);
 
 
 
--- src/WordSearch.java
+++ src/WordSearch.java
@@ -1,3 +1,44 @@
-﻿public class WordSearch {
-    
+﻿import java.io.File; //imports
+import java.io.FileNotFoundException; //have to import in order to pull from a file
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class WordSearch {
+    private char[][] board;
+    private int[] colmoves = {0, 1, 1,  1,  0, -1, -1, -1}; //x
+    private int[] rowmoves = {1, 1, 0, -1, -1, -1,  0,  1}; //y
+
+    public WordSearch(Scanner asd) throws FileNotFoundException {
+        ArrayList<String> lines = new ArrayList<>();
+        try {
+            while(asd.hasNextLine()) {
+                lines.add(asd.nextLine());
+            }
+        } catch (Exception e) {}
+
+        board = new char[lines.size()][lines.get(0).length()];
+        for(int i = 0; i < lines.size(); i++) {
+            for(int j = 0; j < lines.get(i).length(); j++) {
+                board[i][j] = lines.get(i).charAt(j);
+            }
+        }
+        
+    }
+
+
+
+
+
+    @Override
+    public String toString() {
+        String output = "";
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                output+= board[i][j] + " ";
+            }
+            output+= '\n';
+        }
+        return output;
+    }
 }
