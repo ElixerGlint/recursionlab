3aaab3ef89934de1f9e5653b66956bb8cd24099a
Elixer <everett.rumery@gmail.com>
2025-04-07 18:29:04 -0400

    asgasgags

--- src/WordDriver.java
+++ src/WordDriver.java
@@ -4,31 +4,31 @@ import java.util.Scanner;
 
 public class WordDriver {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to the wordsearch solver!");
+        System.out.println("Welcome to the wordsearch solver!"); //introduction
         Scanner input = new Scanner(System.in); //creating a scanner
-        // System.out.println("What is the name of your wordsearch file (make sure to NOT include .txt): "); //asking for the file names (used later to pull and push data)
-        // String file1 = input.next();
-        String file1 ="sample.txt";
+        System.out.println("What is the name of your wordsearch file (make sure to NOT include .txt): "); //asking for the file names (used later to pull and push data)
+        String file1 = input.nextLine();
+        file1+= ".txt";
         Scanner fileinput = new Scanner(new File(file1)); //creating the reader
         WordSearch wordsearch = new WordSearch(fileinput);
         fileinput.close();
-        String cont;
+        String cont; 
 
         System.out.println("What word would you like to find: ");
         cont = input.nextLine();
 
-        while(!cont.equals("!")) {
+        while(!cont.equals("!")) { //while they are not finished
             System.out.println(wordsearch);
-            if(wordsearch.contains(cont)) {
+            if(wordsearch.contains(cont)) { //if there is a soluton: show it
                 System.out.println("The solution is as shown below:");
                 System.out.println(wordsearch);
             }
-            else {
+            else { //otherwise say there is no solution
                 System.out.println("There was no solution");
             }
-            wordsearch.reset();
+            wordsearch.reset(); //reset after every run
             
-            System.out.println("Type the next word you would like to search, otherwise type ! to quit.");
+            System.out.println("Type the next word you would like to search, otherwise type ! to quit."); //ask again
             cont = input.nextLine();
         }
 
--- src/WordSearch.java
+++ src/WordSearch.java
@@ -9,22 +9,32 @@ public class WordSearch {
     private static int[] colmoves = {0, 1, 1,  1,  0, -1, -1, -1}; //x
     private static int[] rowmoves = {1, 1, 0, -1, -1, -1,  0,  1}; //y
 
+    /**
+     * The constructor
+     * @param asd the scanner for the file used
+     * @throws FileNotFoundException the throw exception if no file is found
+     */
     public WordSearch(Scanner asd) throws FileNotFoundException {
-        ArrayList<String> lines = new ArrayList<>();
+        ArrayList<String> lines = new ArrayList<>(); //create the arraylist
         try {
-            while(asd.hasNextLine()) {
+            while(asd.hasNextLine()) { //fills the arraylist
                 lines.add(asd.nextLine());
             }
         } catch (Exception e) {}
 
-        board = new char[lines.size()][lines.get(0).length()];
-        for(int i = 0; i < lines.size(); i++) {
+        board = new char[lines.size()][lines.get(0).length()]; //creates the board
+        for(int i = 0; i < lines.size(); i++) { //fills out the board with the contents of the arraylist
             for(int j = 0; j < lines.get(i).length(); j++) {
                 board[i][j] = lines.get(i).charAt(j);
             }
         }
     }
 
+    /**
+     * This checks if the word is within a wordsearch
+     * @param word the word its looking for
+     * @return true or false, showing if it was found or not
+     */
     public boolean contains(String word) { //works perfectly
         for(int i = 0; i < board.length ; i++) {
             for(int j = 0; j < board[0].length ; j++) {
@@ -38,20 +48,28 @@ public class WordSearch {
         return false;
 	}
 
+    /**
+     * This is the recursive function that solves if it has the word inside of it or not
+     * @param word the word its looking for
+     * @param row the current row
+     * @param col the current collum
+     * @param index what letter of the word they are on (its an integer for ease)
+     * @return true/false if it has been solved
+     */
     public boolean solve(String word, int row, int col, int index) { //WORKS!!!!!!
-        if (index == word.length()) {
+        if (index == word.length()) { //all of the possible ways it can fail
             return true;
         }
         if (!isValid(row, col) || board[row][col] != word.charAt(index)) {
             return false;
         }
     
-        board[row][col] = Character.toUpperCase(board[row][col]);
+        board[row][col] = Character.toUpperCase(board[row][col]); //sets the current space to an uppercase if we know its temporarly correct
     
-        for (int i = 0; i < rowmoves.length; i++) {
+        for (int i = 0; i < rowmoves.length; i++) { //goes through all the moves looking for the correct one
             int nextR = row + rowmoves[i];
             int nextC = col + colmoves[i];
-            if (solve(word, nextR, nextC, index + 1)) {
+            if (solve(word, nextR, nextC, index + 1)) { //if it makes it here, it moves on.
                 return true; 
             }
         }
@@ -61,17 +79,25 @@ public class WordSearch {
         return false;
     }
 
-
+    /**
+     * Checks if the current space is valid to move onto (already used/out of bounds)
+     * @param currentr row
+     * @param currentc collum
+     * @return true or false if its a valid space to make the move onto
+     */
     public boolean isValid(int currentr, int currentc) { //works
-        if(currentr < 0 || currentc < 0 || currentr > board.length - 1 || currentc > board[0].length -1) {
+        if(currentr < 0 || currentc < 0 || currentr > board.length - 1 || currentc > board[0].length -1) { //edges
             return false;
         }
-        if(Character.isUpperCase(board[currentr][currentc])){
+        if(Character.isUpperCase(board[currentr][currentc])){ //uppercase
             return false;
         }
         return true;
     }
 
+    /**
+     * resets the board back to all lowercase
+     */
     public void reset() {
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board[0].length;j++) {
@@ -80,6 +106,9 @@ public class WordSearch {
         }
     }
 
+    /**
+     * The tostring, just prints out the board in a pretty way
+     */
     @Override
     public String toString() {
         String output = "";
