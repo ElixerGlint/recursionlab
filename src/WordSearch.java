378344460ac06884bc3b67c3565b586aebfe2f53
Elixer <everett.rumery@gmail.com>
2025-04-07 17:47:24 -0400

    gasgasgas

--- src/WordSearch.java
+++ src/WordSearch.java
@@ -26,7 +26,6 @@ public class WordSearch {
     }
 
     public boolean start(String word) { //works perfectly
-        System.out.println("Directly below");
         for(int i = 0; i < board.length ; i++) {
             for(int j = 0; j < board[0].length ; j++) {
                 if(board[i][j] == word.charAt(0)) {
